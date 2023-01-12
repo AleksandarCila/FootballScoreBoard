d904b68608b6562542844dbae24e95aadfe14d6b
Aleksandar <aca96@live.co.uk>
2023-01-12 10:38:51 +0100

    Initialized and tested Chakra UI

--- footballscoreboard/src/App.tsx
+++ footballscoreboard/src/App.tsx
@@ -1,7 +1,9 @@
+import { Board } from "./Components";
+
 function App() {
   return (
     <div className="App">
-      
+      <Board />
     </div>
   );
 }
---
+++ footballscoreboard/src/Components/Board/Board.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@chakra-ui/react"
+
+export const Board = () => {
+  return (
+    <Box>Board</Box>
+  )
+}
