eacf23eced8bd48140e9497abb120e6215fca4f7
Aleksandar <aca96@live.co.uk>
2023-01-12 14:07:57 +0100

    Added dispatch actions for starting new game to NewGame component

--- footballscoreboard/src/Components/NewGame/NewGame.tsx
+++ footballscoreboard/src/Components/NewGame/NewGame.tsx
@@ -1,17 +1,30 @@
-import { Box, Flex } from "@chakra-ui/react";
 import { useState } from "react";
 
+import { useDispatch } from "react-redux";
+import { startNewGame } from "../../store/actions";
+
+import { Box, Button, Flex } from "@chakra-ui/react";
 import { BasicInput } from "../Inputs";
+
+import { Game } from "../../Models/Game";
 
 export const NewGame = () => {
   const [homeTeam, setHomeTeam] = useState("");
   const [awayTeam, setAwayTeam] = useState("");
+
+  const dispatch = useDispatch();
+
+  const handleStartNewGame = () => {
+    const newGame = new Game(homeTeam,awayTeam);
+    dispatch(startNewGame(newGame));
+  }
 
   return (
     <Flex justifyContent="space-evenly">
       <BasicInput value={homeTeam} onChange={setHomeTeam} label="Home Team" />
       <Box>-</Box>
       <BasicInput value={awayTeam} onChange={setAwayTeam} label="Away Team" />
+      <Button onClick={handleStartNewGame}>Start</Button>
     </Flex>
   );
 };
